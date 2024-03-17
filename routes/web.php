a45e8090ce1b6007c1546ab03af34cbc263a7c9e
smart <smartmanage715@gmail.com>
2024-03-18 04:32:09 +0900

    Creating The Todo List

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TodoController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return \Inertia\Inertia::render('Home');
-});
+Route::get('/', [HomeController::class, 'index']);
+Route::apiResource('todos', TodoController::class)->except(['index', 'show']);
